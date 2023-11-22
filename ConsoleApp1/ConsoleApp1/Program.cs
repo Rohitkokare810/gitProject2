f30a7e05e691354706421854cbc826137cd910b8
RohitKokare810 <Rohitkokare810@gmail.com>
2023-11-22 20:14:20 +0530

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        
+        List<Student> students = ReadStudentData("F:\\mphasis\\practice projects\\2nd project\\student.txt");
+
+        
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Name: {student.Name}, Age: {student.Age}, Department: {student.Department}");
+        }
+        Console.ReadKey();
+    }
+
+    static List<Student> ReadStudentData(string filePath)
+    {
+        List<Student> students = new List<Student>();
+
+        try
+        {
+            
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines)
+            {
+                
+                string[] parts = line.Split(',');
+
+                
+                Student student = new Student
+                {
+                    Name = parts[0],
+                    Age = int.Parse(parts[1]),
+                    Department = parts[2]
+                };
+
+                
+                students.Add(student);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+
+        return students;
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,6 @@
+﻿public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Department { get; set; }
+}
